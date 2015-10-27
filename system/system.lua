ac4db934a50e1fef942fd3d8be44d0d10a3f94dd
acharluk <acharluk@gmail.com>
2015-10-27 18:40:42 +0100

    Now system runs programs Preparing for user apps

--- system/system.lua
+++ system/system.lua
@@ -1,4 +1,4 @@
-local _build = 4
+local _build = 5
 
 function start()
 	print("Welcome to MaCCOS [build " .. _build .. "]")
@@ -7,9 +7,35 @@ end
 
 function loop()
 	write("\n/" .. Directory.get() .. "> ")
-	System.runProgram(read())
+	runProgram(read())
 end
 
 function stop()
 
+end
+
+
+--[[ System functions ]]--
+runProgram = function(name)
+	if name == nil then return end
+	program = String.split(name)
+
+	sys_app = "/sysapps/" .. tostring(program[1])
+	usr_app = "/usr/apps/" .. tostring(program[1])
+
+	if Directory.isFile(sys_app) then
+		table.remove(program, 1)
+
+		prog = coroutine.create(function()
+			os.run(getfenv(), sys_app, unpack(program))
+		end)
+		coroutine.resume(prog, program)
+
+		return
+	elseif Directory.isDir(usr_app) then
+
+	else
+		print("Program not found.")
+		return
+	end
 end
