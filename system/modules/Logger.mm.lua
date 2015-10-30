6e5d7959a16bfacd56d7bfd3117a829707768675
acharluk <acharluk@gmail.com>
2015-10-30 08:14:46 +0100

    String and Logger modules

---
+++ system/modules/Logger.mm.lua
@@ -0,0 +1,23 @@
+Logger = {
+	file = nil,
+	start = function()
+		file = fs.open(_lastlogfile, "w")
+		return file
+	end,
+	log = function(string)
+		msg = "[" .. os.time() .. "] " .. string
+		print(msg)
+		file.writeLine(msg)
+	end,
+	stop = function()
+		file.close()
+		f = fs.open(_lastlogfile, "r")
+		data = f.readAll()
+		f.close()
+
+		log = fs.open(_logfile, "a")
+		log.writeLine("\n\n/********************/")
+		log.write(data)
+		log.close()
+	end
+}
---
+++ system/modules/String.mm.lua
@@ -0,0 +1,14 @@
+String = {
+	split = function(input, reg)
+		if reg == nil then
+			reg = "%s"
+		end
+		local t = {}
+		i = 1
+		for str in string.gmatch(input, "([^" .. reg .. "]+)") do
+			t[i] = str
+			i = i + 1
+		end
+		return t
+	end
+}
