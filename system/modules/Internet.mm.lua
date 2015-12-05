c8e6aa7ed8fa9ded4e10cc8d63c86019ac366889
acharluk <acharluk@gmail.com>
2015-12-05 14:24:21 +0100

    Added Internet.mm module

---
+++ system/modules/Internet.mm.lua
@@ -0,0 +1,25 @@
+Internet = {
+	DownloadFile = function(path)
+		http.request(path)
+
+		requesting = true
+
+		while requesting do
+			event, url, sourceText = os.pullEvent()
+
+			if event == "http_success" then
+				respondedText = sourceText.readAll()
+
+				sourceText.close()
+
+				requesting = false
+			elseif event == "http_failure" then
+				print("Server didn't respond.")
+
+				requesting = false
+			end
+		end
+		--print(respondedText)
+		return respondedText
+	end
+}
