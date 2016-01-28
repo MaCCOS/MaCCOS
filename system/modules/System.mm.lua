92627ac31bdc42df78eced6659bdd1533e9ad37c
ACharLuk <acharluk@gmail.com>
2016-01-28 21:46:27 +0100

    Added AddToInit function (WIP)

--- system/modules/System.mm.lua
+++ system/modules/System.mm.lua
@@ -1,5 +1,12 @@
 System = {
 	halt = os.shutdown,
 	reboot = os.reboot,
-	exit = function() _running = false end
+	exit = function() _running = false end,
+    
+    AddToInit = function(program)
+        init_file = fs.open("/system/init", "a")
+        init.writeLine("shell.run('" .. program .. "')")
+        init_file.close()
+    end
+    
 }
