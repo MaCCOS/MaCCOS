8513037a3d9bcfda0fb811a9a170717cbe77f722
ACharLuk <acharluk@gmail.com>
2016-01-28 21:46:57 +0100

    Simplified and fixed errors of the Logger module

--- system/modules/Logger.mm.lua
+++ system/modules/Logger.mm.lua
@@ -1,26 +1,30 @@
 _logfile = "/system/logs/logfile.log"
 _lastlogfile = "/system/logs/last.log"
+_logger_started = false
 
 Logger = {
 	file = nil,
 	start = function()
-		file = fs.open(_lastlogfile, "w")
-		return file
+        if not _logger_started then
+		  Logger.file = fs.open(_lastlogfile, "w")
+        end
 	end,
 	log = function(string)
 		msg = "[" .. os.time() .. "] " .. string
 		print(msg)
-		file.writeLine(msg)
+		Logger.file.writeLine(msg)
 	end,
 	stop = function()
-		file.close()
-		f = fs.open(_lastlogfile, "r")
-		data = f.readAll()
-		f.close()
+        if _logger_started then
+            Logger.file.close()
+            f = fs.open(_lastlogfile, "r")
+            data = f.readAll()
+            f.close()
 
-		log = fs.open(_logfile, "a")
-		log.writeLine("\n\n/********************/")
-		log.write(data)
-		log.close()
+            log = fs.open(_logfile, "a")
+            log.writeLine("\n\n/********************/")
+            log.write(data)
+            log.close()
+        end
 	end
 }
