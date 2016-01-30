bf74fbd2d2e67e70cf373449964f1061f585239c
ACharLuk <acharluk@gmail.com>
2016-01-30 23:40:31 +0100

    Added print, println and read functions to System module

--- system/modules/System.mm.lua
+++ system/modules/System.mm.lua
@@ -1,7 +1,28 @@
 System = {
 	halt = os.shutdown,
-	reboot = os.reboot,
+    
+	reboot = function()
+        Logger.stop()
+        os.reboot()
+    end,
+    
 	exit = function() _running = false end,
+    
+    print = function(str)
+        write(str)
+        Logger.log(str)
+    end,
+    
+    println = function(str)
+        System.print(str .. "\n")
+    end,
+    
+    read = function()
+        input = read()
+        Logger.log(input)
+        
+        return input
+    end,
     
     AddToInit = function(program)
         init_file = fs.open("/system/init", "a")
