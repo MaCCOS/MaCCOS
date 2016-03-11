2f80aa4c515633e9aeb75bc3232e96a9d9009a17
ACharLuk <acharluk@gmail.com>
2016-03-12 00:27:23 +0100

    More development on drawPixel

--- system/modules/Screen.mm.lua
+++ system/modules/Screen.mm.lua
@@ -37,9 +37,14 @@ Screen = {
 	},
 	Drawing = {
         drawPixel = function(x, y, bgcol, txcol, str)
+            Screen.Cursor.setPos(x, y)
+            Screen.Cursor.savePos()
+            
             paintutils.drawPixel(x, y, bgcol)
-            write(str)
+            
+            Screen.Cursor.restorePos()
             Screen.Cursor._sp()
+            write(str)
         end,
         drawLine = function(minx, miny, maxx, maxy, color)
             paintutils.drawLine(minx, miny, maxx, maxy, color)
