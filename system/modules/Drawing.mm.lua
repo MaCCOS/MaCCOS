3447376b488dd9b08986208f31db25990a909208
ACharLuk <acharluk@gmail.com>
2016-03-12 10:20:46 +0100

    Added drawRect and setBgColor functions

--- system/modules/Drawing.mm.lua
+++ system/modules/Drawing.mm.lua
@@ -1,4 +1,10 @@
 Drawing = {
+    setBgColor = function(color)
+        Screen.Cursor.savePos()
+        Screen.Color.Bg.set(color)
+        Screen.clear()
+        Screen.Cursor.restorePos()
+    end,
     drawPixel = function(x, y, bgcol, txcol, str)
         Screen.Cursor.setPos(x, y)
         Screen.Cursor.savePos()
@@ -13,5 +19,10 @@ Drawing = {
         paintutils.drawLine(minx, miny, maxx, maxy, color)
         Screen.Cursor._sp()
     end,
-    
+    drawRect = function(minx, miny, maxx, maxy, color)
+        Drawing.drawLine(1, 1, maxx, 1, color)
+        Drawing.drawLine(maxx, 1, maxx, maxy, color)
+        Drawing.drawLine(maxx, maxy, 1, maxy, color)
+        Drawing.drawLine(1, maxy, 1, 1, color)
+    end
 }
