4d34b70fbb7054864c7e078eb2bda9cd7b4a11a3
ACharLuk <acharluk@gmail.com>
2016-04-21 23:27:54 +0200

    Logger and System modules are loaded on preboot to have minimal utilities

--- boot/boot.lua
+++ boot/boot.lua
@@ -1,5 +1,21 @@
 local kernel_file = "/system/kernel"
 local system_file = "/system/system.lua"
 
-local kernel = loadfile(kernel_file)
+_MAC = { _OENV = _G }
+
+_MAC.flags = {
+    ["--silent"] = false,
+    ["--force"] = false,
+	["--debug"] = true
+}
+
+--[[ Manually load Logger and System modules, this should always be loaded ]]--
+loadfile("/system/modules/" .. "Logger" .. ".mm.lua", _MAC)()
+loadfile("/system/modules/System.mm.lua", _MAC)()
+
+_MAC.Logger.start()
+
+local kernel = loadfile(kernel_file, _MAC)
 kernel(system_file)
+
+_MAC.Logger.stop()
