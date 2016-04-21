10a4b1a8c14aff95764336755752361537762b99
ACharLuk <acharluk@gmail.com>
2016-04-22 00:06:26 +0200

    Added debug function, fixed naming

--- system/modules/System.mm.lua
+++ system/modules/System.mm.lua
@@ -13,7 +13,7 @@ System = {
 
     print = function(str)
         if not flags["--silent"] then
-            _OLD_ENV.write(str)
+            _OENV.write(str)
             Logger.log(str)
         end
     end,
@@ -23,16 +23,27 @@ System = {
     end,
 
     read = function()
-        input = _OLD_ENV.read()
+        input = _OENV.read()
         Logger.log(input)
 
         return input
     end,
 
+	debug = function(level, msg)
+		if flags["--debug"] then
+			if     level == 0 then level = "INFO"
+			elseif level == 1 then level = "WARNING"
+			elseif level == 2 then level = "ERROR"
+			elseif level == 3 then level = "FATAL"
+			end
+			System.println("[" .. level .. "] " .. msg)
+		end
+	end,
+
     --[[WIP]]--
     AddToInit = function(program)
         if Directory.exists("/sysapps/" .. program) then
-            init_file = _OLD_ENV.fs.open("/system/init", "a")
+            init_file = _OENV.fs.open("/system/init", "a")
             init_file.writeLine("shell.run('" .. "/sysapps/" .. program .. "')")
             init_file.close()
         else
