09c8d9de73a4775aa6435fab4d8fd794acc04b76
ACharLuk <acharluk@gmail.com>
2016-04-22 23:07:30 +0200

    Now using new GetEnv function

--- system/system.lua
+++ system/system.lua
@@ -1,8 +1,8 @@
-local _build = 12
+local _build = 13
 
 function start()
 	System.println("Welcome to MaCCOS [build " .. _build .. "]")
-    _OENV.os.run(_OENV.getfenv(), "/system/init")
+    _OENV.os.run(Utils.GetEnv(), "/system/init")
 	if not flags["--nogui"] then
 		xstart()
 	end
@@ -28,22 +28,28 @@ runProgram = function(name)
 	if name == nil then return end
 	program = String.split(name)
 
-	sys_app = "/sysapps/" .. _OENV.tostring(program[1])
-	usr_app = "/usr/apps/" .. _OENV.tostring(program[1])
+	sys_app = "/sysapps/" .. Utils.ToString(program[1])
+	usr_app = "/usr/apps/" .. Utils.ToString(program[1])
 
 	if Directory.isFile(sys_app) then
+
 		_OENV.table.remove(program, 1)
-		_OENV.os.run(_OENV.getfenv(), sys_app, _OENV.unpack(program))
+		_OENV.os.run(Utils.GetEnv(), sys_app, _OENV.unpack(program))
 		return
+
 	elseif Directory.isDir(usr_app) then
+
 		prog = _OENV.coroutine.create(function()
-			os.run(_OENV.getfenv(), usr_app .. "/main.lua", _OENV.unpack(program))
+			os.run(Utils.GetEnv(), usr_app .. "/main.lua", _OENV.unpack(program))
 			main()
 		end)
 		_OENV.coroutine.resume(prog, program)
+
 	else
+
 		System.println("Program not found.")
 		return
+
 	end
 end
 
